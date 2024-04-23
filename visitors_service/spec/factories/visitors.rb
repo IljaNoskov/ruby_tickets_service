24b0efd66d7bd1c265bc380e2db388565c6a6e9b
eugen_cherkasov <zcherckasov@gmail.com>
2024-04-23 19:20:21 +0300

    Создана модель посетителя

---
+++ visitors_service/spec/factories/visitors.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :visitor do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    middle_name { "MyString" }
+    document_type { "MyString" }
+    document_number { "MyString" }
+    birthdate { "2024-04-23" }
+  end
+end
