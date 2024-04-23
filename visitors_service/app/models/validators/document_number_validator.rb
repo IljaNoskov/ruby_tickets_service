94959aedaca0ab154d086eddd1eeab40dce13c45
eugen_cherkasov <zcherckasov@gmail.com>
2024-04-23 22:39:39 +0300

    Добавлена валидация для модели посетителя

---
+++ visitors_service/app/models/validators/document_number_validator.rb
@@ -0,0 +1,15 @@
+class DocumentNumberValidator < ActiveModel::Validator
+  DOCUMENTS_FORMATS = {
+    passport: /\A\d{10}\z/,
+    license: /\A(\d{4})|(\d{2}[А-Я]{2})\d{4}\z/,
+    certificate: /\A[IVX]+[А-Я]{2}\d{6}\z/
+  }
+
+  def validate(record)
+    format = DOCUMENTS_FORMATS[record.document_type.to_sym]
+    unless format && format.match?(record.document_number)
+      record.errors.add :document_number, I18n.t(:uncorrect_format)
+    end
+  end
+
+end
--- visitors_service/app/models/visitor.rb
+++ visitors_service/app/models/visitor.rb
@@ -1,2 +1,8 @@
 class Visitor < ApplicationRecord
+  enum document_type: %w[passport license certificate].index_by(&:to_sym)
+  validates :first_name, :last_name, format: { with: /\A[а-яА-Я]+\z/, message: I18n.t(:only_letters) }
+  validates :middle_name, format: {with: /\A[а-яА-Я]+\z/, message: I18n.t(:only_letters) }, if: Proc.new { |v| v.middle_name }
+
+  validates_with DocumentNumberValidator
+
 end
