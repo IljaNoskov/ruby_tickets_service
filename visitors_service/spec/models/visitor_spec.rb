9f29902268ed6824c011c10bb0c74b0393e4139e
eugen_cherkasov <zcherckasov@gmail.com>
2024-04-24 14:42:27 +0300

    Настроен rspec

--- visitors_service/spec/models/visitor_spec.rb
+++ visitors_service/spec/models/visitor_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Visitor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'valid' do
+    let(:visitor) { Visitor.new(first_name: 'Евгений', last_name: 'Черкасов', middle_name: 'Олегович',birthdate: Date.current) }
+
+    it 'with valid attributes'
+    it 'without middle name'
+    it 'with valid license number'
+    it 'with valid birth certificate number'
+  end
+
+  context 'invalid' do
+    it 'with invalid names'
+    it 'with invalid document type'
+    it 'with invalid document number'
+  end
+
 end
