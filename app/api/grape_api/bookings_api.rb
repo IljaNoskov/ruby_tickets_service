ad14a5fbd037bf6f2cdece8dadbf98fd579c71bb
CTAPbIY <reazov02@mail.ru>
2024-04-24 11:04:07 +0000

    add some changes in bookings api

--- app/api/grape_api/bookings_api.rb
+++ app/api/grape_api/bookings_api.rb
@@ -11,23 +11,33 @@ class GrapeApi
         requires :date, type: Date, desc: 'Date of event'
       end
       post do
-        booking = Booking.create(ticket_type: params[:ticket_type], event_id: params[:event_id], booking_number: SecureRandom.uuid, status: 'reserved')
+        booking = Booking.new(ticket_type: params[:ticket_type], event_id: params[:event_id], booking_number: SecureRandom.uuid, status: 'reserved')
+        error!({ message: 'something goes wrong' }, 406) unless booking
+        booking.save
         BookingControlJob.perform_in(5.minute, booking.booking_number)
         present booking, with: GrapeApi::Entities::Booking
       end
 
       params do 
         requires :booking_number, type: String, desc: 'Number of booking'
+        requires :status, type: String, desc: 'Booking status'
       end
       put do
-        Booking.find_by(booking_number: params[:booking_number]).update(status: 'reserved')
+        booking = Booking.find_by(booking_number: params[:booking_number])
+        error!({ message: 'Unavailable booking numver'}, 406) unless booking
+        booking.update(status: params[:status])
+        booking.status == 'reserved' ? BookingControlJob.perform_in(2.minute, params[:booking_number]) : booking
+        { status: 200, message: 'Booking status was successfully updated' }
       end
 
       params do
         requires :booking_number, type: String, desc: 'Number of booking'
       end
       delete do
-        Booking.find_by(booking_number: params[:booking_number]).destroy
+        booking = Booking.find_by(booking_number: params[:booking_number])
+        error!({ message: 'booking not found' }, 404) unless booking
+        booking.destroy
+        { status: 204, message: 'Booking was successfully deleted' } 
       end
 
       params do 
@@ -36,7 +46,7 @@ class GrapeApi
       get do 
         booking = Booking.all
         booking = booking.where('booking_number == :booking_number', booking_number: params[:booking_number]) if params[:booking_number].present?
-        present booking
+        present booking, with: GrapeApi::Entities::Booking
       end
     end
   end
