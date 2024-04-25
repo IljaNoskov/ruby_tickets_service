3852ca21c61c432ba4a1aa18967293d0bbb2a273
CTAPbIY <reazov02@mail.ru>
2024-04-25 13:34:09 +0000

    добавил в put запрос сущность

--- app/api/grape_api/bookings_api.rb
+++ app/api/grape_api/bookings_api.rb
@@ -20,14 +20,13 @@ class GrapeApi
 
       params do 
         requires :booking_number, type: String, desc: 'Number of booking'
-        requires :status, type: String, desc: 'Booking status'
       end
       put do
         booking = Booking.find_by(booking_number: params[:booking_number])
-        error!({ message: 'Unavailable booking numver'}, 406) unless booking
-        booking.update(status: params[:status])
+        error!({ message: 'Unavailable booking number'}, 404) unless booking
+        booking.status == 'reserved' ? booking.update(status: 'payment') : booking.update(status: 'reserved')
         booking.status == 'reserved' ? BookingControlJob.perform_in(2.minute, params[:booking_number]) : booking
-        { status: 200, message: 'Booking status was successfully updated' }
+        { status: 200, message: 'Booking status was successfully updated', body: booking }
       end
 
       params do
@@ -41,11 +40,11 @@ class GrapeApi
       end
 
       params do 
-        optional :booking_number, type: String
+        requires :booking_number, type: String
       end
       get do 
-        booking = Booking.all
-        booking = booking.where('booking_number == :booking_number', booking_number: params[:booking_number]) if params[:booking_number].present?
+        booking = Booking.find_by(booking_number: params[:booking_number])
+        error!({ message: 'Unavailable booking number' }, 404) unless booking
         present booking, with: GrapeApi::Entities::Booking
       end
     end
