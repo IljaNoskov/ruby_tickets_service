acc988808a91b7988fe6dc8ff81d13b05a41a238
eugen_cherkasov <zcherckasov@gmail.com>
2024-04-26 23:00:36 +0300

    Добавлена проверка на nil в sidekiq job

--- booking/app/sidekiq/booking_control_job.rb
+++ booking/app/sidekiq/booking_control_job.rb
@@ -3,6 +3,7 @@ class BookingControlJob
 
   def perform(booking_number)
     booking = Booking.find_by(booking_number: booking_number)
+    return if booking.nil?
     booking.status == 'reserved' ? booking.destroy : booking
   end
 end
