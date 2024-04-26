c2170e8950c3dd3daa85fc027bd9a75ec0c8a85e
eugen_cherkasov <zcherckasov@gmail.com>
2024-04-26 23:57:53 +0300

    Удалил устаревшую копию сервиса бронирования

--- booking_old/app/sidekiq/booking_control_job.rb
+++
@@ -1,8 +0,0 @@
-class BookingControlJob
-  include Sidekiq::Job
-
-  def perform(booking_number)
-    booking = Booking.find_by(booking_number: booking_number)
-    booking.status == 'reserved' ? booking.destroy : booking
-  end
-end
--- booking_old/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  mount GrapeApi => '/api'
-  mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
